4b427b6d0fea05aa0252e49f990b69051d4822ae
VitBoch <vitaliybochkarev@mail.ru>
2022-05-17 17:45:30 +0700

    4 part

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const input = require('sync-input')
 
 const currencies = {
-    'USD': 1.0,
-    'JPY': 113.5,
-    'EUR': 0.89,
-    'RUB': 74.36,
-    'GBP': 0.75
+    USD: 1.0,
+    JPY: 113.5,
+    EUR: 0.89,
+    RUB: 74.36,
+    GBP: 0.75
 }
 
 console.log(`Welcome to Currency Converter!
@@ -13,8 +13,7 @@ console.log(`Welcome to Currency Converter!
 1 USD equals  113.5 JPY
 1 USD equals  0.89 EUR
 1 USD equals  74.36 RUB
-1 USD equals  0.75 GBP
-What do you want to convert?`)
+1 USD equals  0.75 GBP`)
 
 function calculate(amount, from, to) {
     const result = amount * currencies[to] / currencies[from]
@@ -43,22 +42,32 @@ function currencyCheck(data) {
     }
 }
 
+const question = `What do you want to do?
+1-Convert currencies 2-Exit program`
+
 while (true) {
-    let from = currencyCheck('From: > ');
-    if (!from) {
+    console.log(question)
+    const choice = input('> ')
+    if (choice == 2) {
+        console.log('Have a nice day!')
         break
+    } else if (choice != 1) {
+        console.log('Unknown input')
+        continue
     }
-    let to = currencyCheck('To: > ')
+    console.log('What do you want to convert?')
+    const from = currencyCheck('From: > ')
+    if (!from) {
+        continue
+    }
+    const to = currencyCheck('To: > ')
     if (!to) {
-        break
+        continue
     }
     const amount = amountCheck()
     if (!amount) {
-        break
+        continue
     }
     const output = calculate(amount, from, to)
     console.log(`Result: ${amount} ${from} equals ${output} ${to}`)
-    if (output) {
-        break
-    }
 }
